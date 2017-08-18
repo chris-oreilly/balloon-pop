a70159599df0dba4615921dbdd0a98763f7c4efd
Chris O'Reilly <oreillc194@potsdam.edu>
2017-08-18 18:27:11 -0400

    Continuously add balloons

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-var balloon = document.querySelector('.balloon');
+var numBalloons = 0;
+var maxBalloons = 20;
 
-// assume image is square
-var width = 0.1 * Math.min(window.innerWidth, window.innerHeight);
-var height = width;
+function addBalloon() {
+  var balloon = document.createElement('img');
+  balloon.className = 'balloon';
+  balloon.src = 'balloon.svg';
 
-// resize balloon and position it just below the visible area
-TweenMax.set(balloon, {
-  visibility: 'visible',
-  width: width,
-  height: height,
-  x: (window.innerWidth - width * 1.5) * Math.random(),
-  y: window.innerHeight,
-});
+  document.body.appendChild(balloon);
 
-// float steadily upward
-TweenMax.to(balloon, 10, {
-  y: -height,
-  ease: Linear.easeNone,
-  onComplete: function() {
-    TweenMax.killTweensOf(balloon);
-    document.body.removeChild(balloon);
-  },
-});
+  // assume image is square
+  var width = 0.1 * Math.min(window.innerWidth, window.innerHeight);
+  var height = width;
 
-// gently wobble sideways
-TweenMax.to(balloon, 2, {
-  x: '+=' + width * 0.5,
-  ease: Sine.easeInOut,
-  repeat: -1,
-  yoyoEase: true,
+  // resize balloon and position it just below the visible area
+  TweenMax.set(balloon, {
+    visibility: 'visible',
+    width: width,
+    height: height,
+    x: (window.innerWidth - width * 1.5) * Math.random(),
+    y: window.innerHeight,
+  });
+
+  // float steadily upward
+  TweenMax.to(balloon, 10, {
+    y: -height,
+    ease: Linear.easeNone,
+    onComplete: removeBalloon,
+    onCompleteParams: [balloon],
+  });
+
+  // gently wobble sideways
+  TweenMax.to(balloon, 2, {
+    x: '+=' + width * 0.5,
+    ease: Sine.easeInOut,
+    repeat: -1,
+    yoyoEase: true,
+  });
+
+  numBalloons++;
+}
+
+function removeBalloon(balloon) {
+  TweenMax.killTweensOf(balloon);
+  document.body.removeChild(balloon);
+
+  numBalloons--;
+}
+
+TweenMax.ticker.addEventListener('tick', function() {
+  // the fewer balloon on screen, the greater the chance to add more
+  var chance = 0.1 * (maxBalloons - numBalloons) / maxBalloons;
+  if (Math.random() < chance)
+    addBalloon();
 });
