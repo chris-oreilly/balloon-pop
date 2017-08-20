6d1cc4f8a8b7ce69afdafa73852cc2947ed77dae
Chris O'Reilly <oreillc194@potsdam.edu>
2017-08-20 19:19:41 -0400

    Make colors appear more "random"

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ var maxBalloons = 10;
 var numPopped = 0;
 var counter = document.getElementById('counter');
 
+// returns a quasi-random number generator based on the golden ratio
+// idea borrowed from Martin Ankerl
+// martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+function phiRandom() {
+  var phi_conjugate = 0.6180339887498948;
+  var seed = Math.random();
+  return function() {
+    seed = (seed + phi_conjugate) % 1;
+    return seed;
+  }
+}
+
+var randomHue = phiRandom();
+
 function hsla(h, s, l, a) {
   return 'hsla(' +
     Math.round(h * 360) +
@@ -25,7 +39,7 @@ function addBalloon() {
   ctx.bezierCurveTo(88, 74, 51, 100, 50, 100);
   ctx.bezierCurveTo(50, 100, 14, 74, 14, 41);
   ctx.bezierCurveTo(14, 23, 22, 0, 50, 0);
-  var hue = Math.round(Math.random() * 360);
+  var hue = randomHue();
   var gradient = ctx.createRadialGradient(50, 40, 0, 50, 50, 50);
   gradient.addColorStop(0, hsla(hue, 1.0, 0.9, 0.8));
   gradient.addColorStop(1, hsla(hue, 1.0, 0.4, 0.9));
